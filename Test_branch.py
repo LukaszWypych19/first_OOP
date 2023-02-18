ca261b0178e3facd55c6209c6983b58ec8f83d3a
Luk <lukasz.wypych19@gmail.com>
2023-02-18 16:47:25 +0100

    Branches test

--- Test_branch.py
+++ Test_branch.py
@@ -2,3 +2,6 @@ print('Hello first branch')
 print('Hello second branch')
 imie = input('Podaj swoje imie: ')
 print('czesc', imie)
+
+nazwisko = input('podaj swoje nazwisko: ')
+print('czesc', imie, nazwisko)
